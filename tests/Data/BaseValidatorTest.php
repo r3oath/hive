32b50253d998bf8224b7aa09ca6fb59bef9e3408
Tristan Strathearn <r3oath@gmail.com>
2015-09-30 21:17:17 +1000

    Add base validator tests.

---
+++ tests/Data/BaseValidatorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use Mockery as m;
+use R\Hive\Concrete\Data\BaseValidator;
+
+class BaseValidatorTest extends PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testFailedValidation()
+    {
+        $errors = [
+            'something went wrong... duh',
+        ];
+
+        $validator = m::mock('Illuminate\Contracts\Validation\Validator', function ($mock) use ($errors) {
+            $mock->shouldReceive('fails')->atLeast()->once()->andReturn(true);
+            $mock->shouldReceive('errors')->atLeast()->once()->andReturn($errors);
+        });
+
+        $factory = m::mock('Illuminate\Contracts\Validation\Factory', function ($mock) use ($validator) {
+            $mock->shouldReceive('make')->atLeast()->once()->andReturn($validator);
+        });
+
+        $dummy_validator = new BaseValidator($factory);
+        $dummy_validator->validate(['foo' => 'bar']);
+
+        $this->assertEquals(true, $dummy_validator->hasErrors());
+        $this->assertEquals($errors, $dummy_validator->getErrors());
+    }
+
+    public function testSuccessfulValidation()
+    {
+        $validator = m::mock('Illuminate\Contracts\Validation\Validator', function ($mock) {
+            $mock->shouldReceive('fails')->atLeast()->once()->andReturn(false);
+        });
+
+        $factory = m::mock('Illuminate\Contracts\Validation\Factory', function ($mock) use ($validator) {
+            $mock->shouldReceive('make')->atLeast()->once()->andReturn($validator);
+        });
+
+        $dummy_validator = new BaseValidator($factory);
+        $dummy_validator->validate(['foo' => 'bar']);
+
+        $this->assertEquals(false, $dummy_validator->hasErrors());
+    }
+}
