8775be09c7e2816896b421c219194a209100fe15
Tristan Strathearn <r3oath@gmail.com>
2015-10-11 21:51:58 +1000

    Fix invalid class name, and implement abstract method.

--- src/Commands/HiveGeneratorCommand.php
+++ src/Commands/HiveGeneratorCommand.php
@@ -4,7 +4,7 @@ namespace R\Hive\Commands;
 
 use Illuminate\Console\GeneratorCommand;
 
-class MakeValidatorCommand extends GeneratorCommand
+class HiveGeneratorCommand extends GeneratorCommand
 {
     protected function getPath($name)
     {
@@ -12,5 +12,10 @@ class MakeValidatorCommand extends GeneratorCommand
 
         // Add the class type to the file name.
         return $this->laravel['path'].'/'.str_replace('\\', '/', $name).$this->type.'.php';
+    }
+
+    protected function getStub()
+    {
+        return;
     }
 }
