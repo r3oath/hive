c0d7617935e46e52b2445c86c380ab876e090043
Tristan Strathearn <r3oath@gmail.com>
2015-10-12 14:42:18 +1000

    Code cleanup.

--- src/Providers/HiveServiceProvider.php
+++ src/Providers/HiveServiceProvider.php
@@ -4,13 +4,13 @@ namespace R\Hive\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use R\Hive\Commands\AssembleCommand;
+use R\Hive\Commands\MakeCommandCommand;
+use R\Hive\Commands\MakeControllerCommand;
 use R\Hive\Commands\MakeFactoryCommand;
+use R\Hive\Commands\MakeHandlerCommand;
 use R\Hive\Commands\MakeInstanceCommand;
 use R\Hive\Commands\MakeRepoCommand;
 use R\Hive\Commands\MakeValidatorCommand;
-use R\Hive\Commands\MakeControllerCommand;
-use R\Hive\Commands\MakeCommandCommand;
-use R\Hive\Commands\MakeHandlerCommand;
 
 class HiveServiceProvider extends ServiceProvider
 {
